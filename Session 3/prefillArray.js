f92d273c33d6a2783e6a879184e25b0f4cdb8405
Sreelekha Bolloju <Sreelekha_Bolloju@epam.com>
2020-03-13 14:39:46 +0530

    session3 done

---
+++ Session 3/prefillArray.js
@@ -0,0 +1,15 @@
+//http://www.codewars.com/kata/prefill-an-array
+
+function prefill(n, v) {
+  if((!(Number.isInteger(+n))) || (n<0) ||(n===true)||(n===false)){
+  {
+  throw new TypeError(n+" is invalid");
+  }
+ }
+ else if(n==0)
+{
+  return [];
+}
+else
+  return Array(n).fill(v);
+}
---
+++ Session 3/secret.js
@@ -0,0 +1,14 @@
+//http://www.codewars.com/kata/can-you-keep-a-secret
+
+
+function createSecretHolder(secret) {
+var obj={
+    setSecret:function(n){
+    secret=n;
+    },
+    getSecret:function(){
+    return secret;
+    }
+};
+return obj;
+}
