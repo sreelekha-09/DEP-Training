d7f8742c87bb8e730430fa9fb35d311b43152b35
Sreelekha Bolloju <Sreelekha_Bolloju@epam.com>
2020-03-26 19:15:54 +0530

    session 3

---
+++ Closures.js
@@ -0,0 +1,13 @@
+//http://www.codewars.com/kata/closures-and-scopes/train/javascript
+
+function createFunctions(n) {
+  var callbacks = [];
+
+  for (var i = 0; i < n; i++) {
+    callbacks.push(function () {
+      return n - (i--);
+    });
+  }
+
+  return callbacks;
+}
